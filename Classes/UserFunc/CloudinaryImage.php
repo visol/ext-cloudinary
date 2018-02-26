6f35f9232383330fa9653d110c3088968f365f55
Jonas Renggli <jonas.renggli@visol.ch>
2018-02-26 14:02:16 +0100

    [FEATURE] Return secure URL in UserFunc

--- Classes/UserFunc/CloudinaryImage.php
+++ Classes/UserFunc/CloudinaryImage.php
@@ -17,18 +17,60 @@ class CloudinaryImage  {
         $this->cloudinaryUtility = GeneralUtility::makeInstance('Sinso\\Cloudinary\\Utility\\CloudinaryUtility');
     }
 
+    /**
+     * Compress image and return URL (http://)
+     *
+     * @param string $content Source image
+     * @param array $conf
+     *
+     * @return bool
+     * @throws \Sinso\Cloudinary\CloudinaryException
+     */
     public function getUrl($content='', $conf = [])
     {
         if (empty($content)) {
             return false;
         }
-        $imageUri = $content;
+
+        $imageData = $this->getImageData($content, $conf);
+
+        return $imageData->url;
+    }
+
+    /**
+     * Compress image and return secure image URL (https://)
+     *
+     * @param string $content Source image
+     * @param array $conf
+     *
+     * @return bool
+     * @throws \Sinso\Cloudinary\CloudinaryException
+     */
+    public function getSecureUrl($content='', $conf = [])
+    {
+        if (empty($content)) {
+            return false;
+        }
+
+        $imageData = $this->getImageData($content, $conf);
+
+        return $imageData->secure_url;
+    }
+
+    /**
+     * @param $imageUri
+     * @param $conf
+     *
+     * @return mixed
+     * @throws \Sinso\Cloudinary\CloudinaryException
+     */
+    protected function getImageData($imageUri, $conf)
+    {
         $publicId = $this->cloudinaryUtility->getPublicId(ltrim($imageUri, '/'));
         $options = $this->cloudinaryUtility->generateOptionsFromSettings($conf);
         $imageData = current($this->cloudinaryUtility->getResponsiveBreakpointData($publicId,$options));
 
-        return $imageData->url;
-
+        return $imageData;
     }
 
 }
