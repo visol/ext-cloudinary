683dc168ddff3c2dca2e0d594a2fbfa9bce79010
Fabien Udriot <fabien@omic.ch>
2019-11-07 20:30:02 +0100

    [FEATURE] Add Command cloudinary:move (refs #20871)

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,6 +1,9 @@
 <?php
 return [
     'cloudinary:copy' => [
-        'class' => \Sinso\Cloudinary\Command\CloudinaryCommand::class,
+        'class' => \Sinso\Cloudinary\Command\CloudinaryCopyCommand::class,
+    ],
+    'cloudinary:move' => [
+        'class' => \Sinso\Cloudinary\Command\CloudinaryMoveCommand::class,
     ],
 ];
