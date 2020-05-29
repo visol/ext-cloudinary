cea28cec94a7e83a19e55f8a40d17d2dd8e86673
Fabien Udriot <fabien.udriot@visol.ch>
2020-05-29 18:08:55 +0200

    [TASK] Add acceptance tests

---
+++ Tests/Acceptance/CloudinarySimpleSuiteTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Visol\Cloudinary\Tests\Acceptance;
+
+class CloudinarySimpleSuiteTest
+{
+    // todo... setup acceptance | feature test environment
+    // Function to tests
+    // When testing consider to compare speed with the LocalDriver
+    //public function getPublicUrl($fileIdentifier)
+    //public function getFileInfoByIdentifier($fileIdentifier, array $propertiesToExtract = [])
+    //public function fileExists($identifier)
+    //public function folderExists($folderIdentifier)
+    //public function fileExistsInFolder($fileName, $folderIdentifier)
+    //public function folderExistsInFolder($folderName, $folderIdentifier)
+    //public function getFolderInFolder($folderName, $folderIdentifier)
+    //public function addFile($localFilePath, $targetFolderIdentifier, $newFileName = '', $removeOriginal = true)
+    //public function moveFileWithinStorage($fileIdentifier, $targetFolderIdentifier, $newFileName)
+    //public function copyFileWithinStorage($fileIdentifier, $targetFolderIdentifier, $fileName)
+    //public function replaceFile($fileIdentifier, $localFilePath)
+    //public function deleteFile($fileIdentifier)
+    //public function deleteFolder($folderIdentifier, $deleteRecursively = false)
+    //public function getFileForLocalProcessing($fileIdentifier, $writable = true)
+    //public function createFile($fileName, $parentFolderIdentifier)
+    //public function createFolder($newFolderName, $parentFolderIdentifier = '', $recursive = false)
+    //public function getFileContents($fileIdentifier)
+    //public function setFileContents($fileIdentifier, $contents)
+    //public function renameFile($fileIdentifier, $newFileIdentifier)
+    //public function isFileIdentifier(string $newFileIdentifier): bool
+    //public function renameFolder($folderIdentifier, $newFolderName)
+    //public function moveFolderWithinStorage($sourceFolderIdentifier, $targetFolderIdentifier, $newFolderName)
+    //public function copyFolderWithinStorage($sourceFolderIdentifier, $targetFolderIdentifier, $newFolderName)
+    //public function isFolderEmpty($folderIdentifier)
+    //public function isWithin($folderIdentifier, $identifier)
+    //public function getFolderInfoByIdentifier($folderIdentifier)
+    //public function getFileInFolder($fileName, $folderIdentifier)
+    //public function getFilesInFolder($folderIdentifier, $start = 0, $numberOfItems = 40, $recursive = false, array $filenameFilterCallbacks = [], $sort = '', $sortRev = false)
+    //public function countFilesInFolder($folderIdentifier, $recursive = false, array $filenameFilterCallbacks = [])
+    //public function getFoldersInFolder($folderIdentifier, $start = 0, $numberOfItems = 40, $recursive = false, array $folderNameFilterCallbacks = [], $sort = '', $sortRev = false)
+    //public function countFoldersInFolder($folderIdentifier, $recursive = false, array $folderNameFilterCallbacks = [])
+    //public function dumpFileContents($identifier)
+
+    // todo also test speed and cache performance
+
+}
