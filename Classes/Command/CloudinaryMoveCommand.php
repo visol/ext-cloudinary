visol/ext-cloudinary

commit 5e97f0d72a749b4edcd05b62179269b70251ff16

[TASK] Refactor Commands

Classes/Command/CloudinaryMoveCommand.php
@@ -9,6 +9,8 @@ namespace Visol\Cloudinary\Command;
  * LICENSE.md file that was distributed with this source code.
  */
 
+use Exception;
+use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\PathUtility;
 use Visol\Cloudinary\Services\FileMoveService;
 use Symfony\Component\Console\Input\InputArgument;
@@ -33,6 +35,21 @@ class CloudinaryMoveCommand extends AbstractCloudinaryCommand
      * @var array
      */
     protected $skippedFiles;
+
+    /**
+     * @var array
+     */
+    protected $missingFiles = [];
+
+    /**
+     * @var ResourceStorage
+     */
+    protected $sourceStorage;
+
+    /**
+     * @var ResourceStorage
+     */
+    protected $targetStorage;
 
     /**
      * Configure the command by defining the name, options and arguments
@@ -113,15 +130,17 @@ class CloudinaryMoveCommand extends AbstractCloudinaryCommand
      *
      * @param InputInterface $input
      * @param OutputInterface $output
+     *
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if (!$this->checkDriverType()) {
+        if (!$this->checkDriverType($this->targetStorage)) {
             $this->log('Look out! target storage is not of type "cloudinary"');
             return 1;
         }
 
-        $files = $this->getFiles($input);
+        $files = $this->getFiles($this->sourceStorage, $input);
 
         if (count($files) === 0) {
             $this->log('No files found, no work for me!');
@@ -195,7 +214,7 @@ class CloudinaryMoveCommand extends AbstractCloudinaryCommand
                     );
                     $timeElapsedSeconds = microtime(true) - $start;
                     $this->log('File uploaded, Elapsed time %.3f', [$timeElapsedSeconds]);
-                } catch (\Exception $e) {
+                } catch (Exception $e) {
                     $this->log('Mmm..., I could not upload file %s. Exception %s: %s', [$fileObject->getIdentifier(), $e->getCode(), $e->getMessage()], self::WARNING);
                     $this->faultyUploadedFiles[] = $fileObject->getIdentifier();
                     continue;

Classes/Command/AbstractCloudinaryCommand.php
@@ -10,6 +10,7 @@ namespace Visol\Cloudinary\Command;
  */
 
 use Doctrine\DBAL\Driver\Connection;
+use Sinso\Smartimport\Domain\Model\Resource;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -42,56 +43,24 @@ abstract class AbstractCloudinaryCommand extends Command
     protected $isSilent = false;
 
     /**
-     * @var ResourceStorage
-     */
-    protected $sourceStorage;
-
-    /**
-     * @var ResourceStorage
-     */
-    protected $targetStorage;
-
-    /**
      * @var string
      */
     protected $tableName = 'sys_file';
 
     /**
-     * @var array
-     */
-    protected $missingFiles = [];
-
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-        $this->io = new SymfonyStyle($input, $output);
-
-        $this->isSilent = $input->getOption('silent');
-
-        $this->sourceStorage = ResourceFactory::getInstance()->getStorageObject(
-            $input->getArgument('source')
-        );
-        $this->targetStorage = ResourceFactory::getInstance()->getStorageObject(
-            $input->getArgument('target')
-        );
-    }
-
-    /**
+     * @param ResourceStorage $storage
      * @param InputInterface $input
      *
      * @return array
      */
-    protected function getFiles(InputInterface $input): array
+    protected function getFiles(ResourceStorage $storage, InputInterface $input): array
     {
         $query = $this->getQueryBuilder();
         $query
             ->select('*')
             ->from($this->tableName)
             ->where(
-                $query->expr()->eq('storage', $this->sourceStorage->getUid()),
+                $query->expr()->eq('storage', $storage->getUid()),
                 $query->expr()->eq('missing', 0)
             );
 
@@ -176,11 +145,13 @@ abstract class AbstractCloudinaryCommand extends Command
     }
 
     /**
+     * @param ResourceStorage $storage
+     *
      * @return bool
      */
-    protected function checkDriverType(): bool
+    protected function checkDriverType(ResourceStorage $storage): bool
     {
-        return $this->targetStorage->getDriverType() === CloudinaryDriver::DRIVER_TYPE;
+        return $storage->getDriverType() === CloudinaryDriver::DRIVER_TYPE;
     }
 
     /**

Classes/Command/CloudinaryAcceptanceTestCommand.php
@@ -46,7 +46,6 @@ spl_autoload_register(
  */
 class CloudinaryAcceptanceTestCommand extends AbstractCloudinaryCommand
 {
-
     /**
      * Configure the command by defining the name, options and arguments
      */
@@ -63,7 +62,7 @@ class CloudinaryAcceptanceTestCommand extends AbstractCloudinaryCommand
                 'The API configuration'
             )
             ->setHelp(
-                'Usage: ./vendor/bin/typo3 cloudinary:run-tests '
+                'Usage: ./vendor/bin/typo3 cloudinary:tests'
             );
     }
 

Classes/Command/CloudinaryCopyCommand.php
@@ -13,17 +13,51 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Resource\DuplicationBehavior;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use Visol\Cloudinary\Driver\CloudinaryDriver;
 
 /**
  * Class CloudinaryCopyCommand
  */
 class CloudinaryCopyCommand extends AbstractCloudinaryCommand
 {
+
+    /**
+     * @var array
+     */
+    protected $missingFiles = [];
+
+    /**
+     * @var ResourceStorage
+     */
+    protected $sourceStorage;
+
+    /**
+     * @var ResourceStorage
+     */
+    protected $targetStorage;
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->isSilent = $input->getOption('silent');
+
+        $this->sourceStorage = ResourceFactory::getInstance()->getStorageObject(
+            $input->getArgument('source')
+        );
+        $this->targetStorage = ResourceFactory::getInstance()->getStorageObject(
+            $input->getArgument('target')
+        );
+    }
 
     /**
      * Configure the command by defining the name, options and arguments
@@ -107,12 +141,12 @@ class CloudinaryCopyCommand extends AbstractCloudinaryCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 
-        if (!$this->checkDriverType()) {
+        if (!$this->checkDriverType($this->targetStorage)) {
             $this->log('Look out! target storage is not of type "cloudinary"');
             return 1;
         }
 
-        $files = $this->getFiles($input);
+        $files = $this->getFiles($this->sourceStorage, $input);
 
         if (count($files) === 0) {
             $this->log('No files found, no work for me!');

Classes/Command/CloudinaryFixJpegCommand.php
@@ -15,21 +15,18 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Resource\ResourceStorage;
 
 /**
  * Class CloudinaryFixJpegCommand
  */
 class CloudinaryFixJpegCommand extends AbstractCloudinaryCommand
 {
-    /**
-     * @var array
-     */
-    protected $faultyUploadedFiles;
 
     /**
-     * @var array
+     * @var ResourceStorage
      */
-    protected $skippedFiles;
+    protected $targetStorage;
 
     /**
      * @param InputInterface $input
@@ -85,16 +82,18 @@ class CloudinaryFixJpegCommand extends AbstractCloudinaryCommand
      *
      * @param InputInterface $input
      * @param OutputInterface $output
+     *
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 
-        if (!$this->checkDriverType()) {
+        if (!$this->checkDriverType($this->targetStorage)) {
             $this->log('Look out! target storage is not of type "cloudinary"');
             return 1;
         }
 
-        $files = $this->getJpegFiles($input);
+        $files = $this->getJpegFiles();
 
 
         if (count($files) === 0) {
@@ -136,11 +135,9 @@ WHERE storage = " . $this->targetStorage->getUid();
     }
 
     /**
-     * @param InputInterface $input
-     *
      * @return array
      */
-    protected function getJpegFiles(InputInterface $input): array
+    protected function getJpegFiles(): array
     {
         $query = $this->getQueryBuilder();
         $query

Classes/Command/CloudinaryQueryCommand.php
@@ -14,15 +14,16 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Resource\Folder;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Visol\Cloudinary\Services\CloudinaryScanService;
 
 /**
- * Class CloudinaryScanCommand
+ * Class CloudinaryQueryCommand
  */
-class CloudinaryScanCommand extends AbstractCloudinaryCommand
+class CloudinaryQueryCommand extends AbstractCloudinaryCommand
 {
 
     /**
@@ -63,8 +64,8 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
                 false
             )
             ->addOption(
-                'empty',
-                'e',
+                'folder',
+                'f',
                 InputOption::VALUE_OPTIONAL,
                 'Before scanning empty all resources for a given storage',
                 false
@@ -81,6 +82,11 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
                 InputArgument::REQUIRED,
                 'Storage identifier'
             )
+            ->addArgument(
+                'action',
+                InputArgument::REQUIRED,
+                'Possible action "list"'
+            )
             ->setHelp(
                 'Usage: ./vendor/bin/typo3 cloudinary:scan [0-9]'
             );
@@ -89,8 +95,6 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     *
-     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
@@ -99,60 +103,53 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
             return 1;
         }
 
-        if ($input->getOption('empty') === null || $input->getOption('empty')) {
-            $this->log('Emptying all resources for storage %s"', [$this->storage->getUid()]);
-            $this->getCloudinaryScanService()->empty();
-        }
 
-        $result = $this->getCloudinaryScanService()->scan();
+        $folder = $input->getOption('folder');
+        if ($folder === null || $folder) {
 
+            $folderIdentifier = '/'; // we can have a filter here
 
-        $numberOfFiles = $result['created'] + $result['updated'] - $result['deleted'];
-        if ($numberOfFiles !== $result['total']) {
-            $this->error(
-                'Something went wrong. There is a problem with the number of files counted. %s !== %s',
-                [
-                    $numberOfFiles,
-                    $result['total']
-                ]
+            $folders = $this->storage->getFoldersInFolder(
+                $this->getFolder($folderIdentifier)
             );
-        }
 
-        $numberOfFolders = $result['folder_created'] + $result['folder_updated'] - $result['folder_deleted'];
-        if ($numberOfFolders !== $result['folder_total']) {
-            $this->error(
-                'Something went wrong. There is a problem with the number of folders counted. %s !== %s',
-                [
-                    $numberOfFolders,
-                    $result['folder_total']
-                ]
+            foreach ($folders as $folder) {
+                $this->log($folder->getIdentifier());
+            }
+        } else {
+
+            $folderIdentifier = '/'; // we can have a filter here
+
+            $files = $this->storage->getFilesInFolder(
+                $this->getFolder($folderIdentifier)
             );
-        }
 
-        $message = "Statistics for files: \n\n- created: %s\n- updated: %s\n- total: %s\n- deleted: %s";
-        $message .= "\n\nStatistics for folders: \n\n- created: %s\n- updated: %s\n- total: %s\n- deleted: %s";
-        $this->success(
-            $message,
-            [
-                $result['created'],
-                $result['updated'],
-                $result['total'],
-                $result['deleted'],
-                $result['folder_created'],
-                $result['folder_updated'],
-                $result['folder_total'],
-                $result['folder_deleted'],
-            ]
-        );
+            foreach ($files as $file) {
+                $this->log($file->getIdentifier());
+            }
+        }
 
         return 0;
     }
 
     /**
-     * @return object|CloudinaryScanService
+     * @param string $folderIdentifier
+     *
+     * @return object|Folder
      */
-    protected function getCloudinaryScanService(): CloudinaryScanService
+    protected function getFolder($folderIdentifier): Folder
     {
-        return GeneralUtility::makeInstance(CloudinaryScanService::class, $this->storage);
+        $folderIdentifier = $folderIdentifier === DIRECTORY_SEPARATOR
+            ? $folderIdentifier
+            : DIRECTORY_SEPARATOR . trim($folderIdentifier, '/') . DIRECTORY_SEPARATOR;
+
+        return GeneralUtility::makeInstance(
+            Folder::class,
+            $this->storage,
+            $folderIdentifier,
+            $folderIdentifier === DIRECTORY_SEPARATOR
+                ? ''
+                : $folderIdentifier
+        );
     }
 }

Configuration/Commands.php
@@ -6,7 +6,7 @@ return [
     'cloudinary:move' => [
         'class' => \Visol\Cloudinary\Command\CloudinaryMoveCommand::class,
     ],
-    'cloudinary:run-tests' => [
+    'cloudinary:tests' => [
         'class' => \Visol\Cloudinary\Command\CloudinaryAcceptanceTestCommand::class,
     ],
     'cloudinary:fix' => [
@@ -14,5 +14,8 @@ return [
     ],
     'cloudinary:scan' => [
         'class' => \Visol\Cloudinary\Command\CloudinaryScanCommand::class,
+    ],
+    'cloudinary:query' => [
+        'class' => \Visol\Cloudinary\Command\CloudinaryQueryCommand::class,
     ],
 ];

commit 1d55a51b551002d9e544f15158155ca8bcacfd27

[BUGFIX] Initialize arguments in CloudinaryMoveCommand
Regression from 5e97f0d

Classes/Command/CloudinaryMoveCommand.php
@@ -10,6 +10,7 @@ namespace Visol\Cloudinary\Command;
  */
 
 use Exception;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\PathUtility;
 use Visol\Cloudinary\Services\FileMoveService;
@@ -123,6 +124,24 @@ class CloudinaryMoveCommand extends AbstractCloudinaryCommand
             ->setHelp(
                 'Usage: ./vendor/bin/typo3 cloudinary:move 1 2'
             );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->isSilent = $input->getOption('silent');
+
+        $this->sourceStorage = ResourceFactory::getInstance()->getStorageObject(
+            $input->getArgument('source')
+        );
+        $this->targetStorage = ResourceFactory::getInstance()->getStorageObject(
+            $input->getArgument('target')
+        );
     }
 
     /**