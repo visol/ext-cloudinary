0d0a728fd13b9fb96a7d41e3915d242bf580056f
Jonas Renggli <jonas.renggli@visol.ch>
2021-07-19 19:59:37 +0200

    [TASK] Refactor ExplicitData and ExplicitDataCacheRepository

--- Classes/Services/CloudinaryImageService.php
+++ Classes/Services/CloudinaryImageService.php
@@ -14,7 +14,7 @@ use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use Visol\Cloudinary\Domain\Repository\ResponsiveBreakpointsRepository;
+use Visol\Cloudinary\Domain\Repository\ExplicitDataCacheRepository;
 use Visol\Cloudinary\Driver\CloudinaryDriver;
 
 /**
@@ -24,10 +24,10 @@ class CloudinaryImageService
 {
 
     /**
-     * @var ResponsiveBreakpointsRepository
+     * @var ExplicitDataCacheRepository
      * @inject
      */
-    protected $responsiveBreakpointsRepository;
+    protected $explicitDataCacheRepository;
 
     /**
      * @var \TYPO3\CMS\Core\Resource\StorageRepository
@@ -40,34 +40,7 @@ class CloudinaryImageService
      */
     public function __construct()
     {
-        $this->responsiveBreakpointsRepository = GeneralUtility::makeInstance(ResponsiveBreakpointsRepository::class);
-    }
-
-    /**
-     * @param File $file
-     * @param array $options
-     *
-     * @return array
-     */
-    public function getResponsiveBreakpointData(File $file, array $options): array
-    {
-        $publicId = $this->getPublicIdForFile($file);
-
-        $responsiveBreakpoints = $this->responsiveBreakpointsRepository->findByPublicIdAndOptions($publicId, $options);
-
-        if (!$responsiveBreakpoints) {
-            $this->initializeApi($file->getStorage());
-            $response = \Cloudinary\Uploader::explicit($publicId, $options);
-            $breakpointData = json_encode($response['responsive_breakpoints'][0]['breakpoints']);
-            $this->responsiveBreakpointsRepository->save($publicId, $options, $breakpointData);
-        } else {
-            $breakpointData = $responsiveBreakpoints['breakpoints'];
-        }
-
-        $breakpoints = json_decode($breakpointData);
-        return is_array($breakpoints)
-            ? $breakpoints
-            : [];
+        $this->explicitDataCacheRepository = GeneralUtility::makeInstance(ExplicitDataCacheRepository::class);
     }
 
 
@@ -81,21 +54,28 @@ class CloudinaryImageService
     {
         $publicId = $this->getPublicIdForFile($file);
 
-        $responsiveBreakpoints = $this->responsiveBreakpointsRepository->findByPublicIdAndOptions($publicId, $options);
+        $explicitData = $this->explicitDataCacheRepository->findByPublicIdAndOptions($publicId, $options)['explicit_data'];
 
-        if (!$responsiveBreakpoints) {
+        if (!$explicitData) {
             $this->initializeApi($file->getStorage());
-            $response = \Cloudinary\Uploader::explicit($publicId, $options);
-            $breakpointData = json_encode($response);
-            $this->responsiveBreakpointsRepository->save($publicId, $options, $breakpointData);
-        } else {
-            $breakpointData = $responsiveBreakpoints['breakpoints'];
+            $explicitData = \Cloudinary\Uploader::explicit($publicId, $options);
+            $this->explicitDataCacheRepository->save($publicId, $options, $explicitData);
         }
 
-        $breakpoints = json_decode($breakpointData, true);
-        return is_array($breakpoints)
-            ? $breakpoints
-            : [];
+        return $explicitData;
+    }
+
+    /**
+     * @param File $file
+     * @param array $options
+     *
+     * @return array
+     */
+    public function getResponsiveBreakpointData(File $file, array $options): array
+    {
+        $explicitData = $this->getExplicitData($file, $options);
+
+        return $explicitData['responsive_breakpoints'][0]['breakpoints'];
     }
 
     /**
@@ -146,7 +126,7 @@ class CloudinaryImageService
         $imageObjects = $this->getImageObjects($breakpoints);
         $srcset = [];
         foreach ($imageObjects as $imageObject) {
-            $srcset[] = $imageObject->secure_url . ' ' . $imageObject->width . 'w';
+            $srcset[] = $imageObject['secure_url'] . ' ' . $imageObject['width'] . 'w';
         }
 
         return $srcset;
@@ -160,7 +140,7 @@ class CloudinaryImageService
     public function getSizesAttribute(array $breakpoints): string
     {
         $maxImageObject = $this->getImage($breakpoints, 'max');
-        return '(max-width: ' . $maxImageObject->width . 'px) 100vw, ' . $maxImageObject->width . 'px';
+        return '(max-width: ' . $maxImageObject['width'] . 'px) 100vw, ' . $maxImageObject['width'] . 'px';
     }
 
     /**
@@ -205,7 +185,7 @@ class CloudinaryImageService
     {
         $widthMap = [];
         foreach ($breakpoints as $breakpoint) {
-            $widthMap[$breakpoint->width] = $breakpoint;
+            $widthMap[$breakpoint['width']] = $breakpoint;
         }
 
         return $widthMap;
