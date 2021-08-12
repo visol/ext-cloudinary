187864f0a37feef9f3d84fff0e32829934f92ebf
Jonas Renggli <jonas.renggli@visol.ch>
2021-08-12 15:22:50 +0200

    [BUGFIX] Check driver type before processing thumbnails in FileProcessingSlot

--- Classes/Slots/FileProcessingSlot.php
+++ Classes/Slots/FileProcessingSlot.php
@@ -17,6 +17,7 @@ use TYPO3\CMS\Core\Resource\ProcessedFile;
 use TYPO3\CMS\Core\Resource\ProcessedFileRepository;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Visol\Cloudinary\Driver\CloudinaryFastDriver;
 use Visol\Cloudinary\Services\CloudinaryImageService;
 use Visol\Cloudinary\Services\CloudinaryPathService;
 use Visol\Cloudinary\Services\CloudinaryResourceService;
@@ -27,43 +28,47 @@ class FileProcessingSlot
     // We want to remove all processed files
     public function preFileProcess(\TYPO3\CMS\Core\Resource\Service\FileProcessingService $fileProcessingService, DriverInterface $driver, ProcessedFile $processedFile, File $file, $taskType, array $configuration)
     {
-        if (!$processedFile->isProcessed()) {
-            if (strpos($processedFile->getIdentifier() ?? '', 'PROCESSEDFILE' ) === 0) {
-                return;
-            }
-
-            $options = [
-                'type' => 'upload',
-//                'eager_async' => true,
-                'eager' => [
-                    [
-                        //'format' => 'jpg', // `Invalid transformation component - auto`
-                        'fetch_format' => 'auto',
-                        'quality' => 'auto:eco',
-                        'width' => 64,
-                        'height' => 64,
-                        'crop' => 'fit',
-                    ]
-                ]
-            ];
-
-            $explicitData = $this->getCloudinaryImageService()->getExplicitData($file, $options);
-            $url = $explicitData['eager'][0]['secure_url'];
-
-            $parts = parse_url($url);
-            $processedFile->setName(basename($url));
-            $processedFile->setIdentifier('PROCESSEDFILE' . $parts['path']);
-
-            $processedFile->updateProperties([
-                'width' => $explicitData['eager'][0]['width'],
-                'height' => $explicitData['eager'][0]['height'],
-            ]);
-
-            /** @var $processedFileRepository ProcessedFileRepository */
-            $processedFileRepository = GeneralUtility::makeInstance(ProcessedFileRepository::class);
-            $processedFileRepository->add($processedFile);
-
+        if (!$driver instanceof CloudinaryFastDriver) {
+            return;
         }
+
+        if ($processedFile->isProcessed()) {
+            return;
+        }
+
+        if (strpos($processedFile->getIdentifier() ?? '', 'PROCESSEDFILE' ) === 0) {
+            return;
+        }
+
+        $options = [
+            'type' => 'upload',
+            'eager' => [
+                [
+                    //'format' => 'jpg', // `Invalid transformation component - auto`
+                    'fetch_format' => 'auto',
+                    'quality' => 'auto:eco',
+                    'width' => 64,
+                    'height' => 64,
+                    'crop' => 'fit',
+                ]
+            ]
+        ];
+
+        $explicitData = $this->getCloudinaryImageService()->getExplicitData($file, $options);
+        $url = $explicitData['eager'][0]['secure_url'];
+
+        $parts = parse_url($url);
+        $processedFile->setName(basename($url));
+        $processedFile->setIdentifier('PROCESSEDFILE' . $parts['path']);
+
+        $processedFile->updateProperties([
+            'width' => $explicitData['eager'][0]['width'],
+            'height' => $explicitData['eager'][0]['height'],
+        ]);
+
+        /** @var $processedFileRepository ProcessedFileRepository */
+        $processedFileRepository = GeneralUtility::makeInstance(ProcessedFileRepository::class);
+        $processedFileRepository->add($processedFile);
     }
 
     /**
