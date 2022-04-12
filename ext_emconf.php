c9216cbd5adf81ec650bdaa487c91709c676104b
Fabien Udriot <fabarea@users.noreply.github.com>
2022-04-12 12:25:11 +0200

    [TASK] TYPO3 v11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,14 +9,4 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Jonas Renggli',
     'author_email' => 'jonas.renggli@visol.ch',
     'author_company' => 'visol - digitale Dienstleistungen GmbH',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.7.0-8.7.99',
-            'php' => '7.0.0-0.0.0',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ],
 ];
