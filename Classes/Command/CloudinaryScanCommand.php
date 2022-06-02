cc2f19ab604d4c46991273bdf5aed5dbd9d23b9f
Fabien Udriot <fabien@omic.ch>
2022-06-02 15:43:44 +0200

    [ENHANCE] Message consistency in scan service

--- Classes/Command/CloudinaryScanCommand.php
+++ Classes/Command/CloudinaryScanCommand.php
@@ -50,7 +50,13 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
         $message = 'Scan and warm up a cloudinary storage.';
         $this->setDescription($message)
             ->addOption('silent', 's', InputOption::VALUE_OPTIONAL, 'Mute output as much as possible', false)
-            ->addOption('empty', 'e', InputOption::VALUE_OPTIONAL, 'Before scanning empty all resources for a given storage', false)
+            ->addOption(
+                'empty',
+                'e',
+                InputOption::VALUE_OPTIONAL,
+                'Before scanning empty all resources for a given storage',
+                false,
+            )
             ->addArgument('storage', InputArgument::REQUIRED, 'Storage identifier')
             ->setHelp('Usage: ./vendor/bin/typo3 cloudinary:scan [0-9]');
     }
@@ -76,12 +82,21 @@ class CloudinaryScanCommand extends AbstractCloudinaryCommand
 
         $numberOfFiles = $result['created'] + $result['updated'] - $result['deleted'];
         if ($numberOfFiles !== $result['total']) {
-            $this->error('Something went wrong. There is a problem with the number of files counted. %s !== %s', [$numberOfFiles, $result['total']]);
+            $this->error(
+                'Something went wrong. There is a problem with the number of files counted. %s !== %s. It should be fixed in the next scan',
+                [$numberOfFiles, $result['total']],
+            );
         }
 
         $message = "Statistics for files: \n\n- created: %s\n- updated: %s\n- total: %s\n- deleted: %s";
         $message .= "\n\nStatistics for folders: \n\n- deleted: %s";
-        $this->success($message, [$result['created'], $result['updated'], $result['total'], $result['deleted'], $result['folder_deleted']]);
+        $this->success($message, [
+            $result['created'],
+            $result['updated'],
+            $result['total'],
+            $result['deleted'],
+            $result['folder_deleted'],
+        ]);
 
         return 0;
     }
