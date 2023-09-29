b79fb53b7436701c68c9d71060036ca2a68f53d2
Daniel Huf <daniel.huf@visol.ch>
2023-09-29 13:51:01 +0200

    chore: Refactor a bit

--- Resources/Public/JavaScript/CloudinaryMediaLibrary.js
+++ Resources/Public/JavaScript/CloudinaryMediaLibrary.js
@@ -9,21 +9,16 @@ define([
   '//media-library.cloudinary.com/global/all.js',
 ], function ($, NProgress, MessageUtility, Modal, Severity) {
 
-  $('.btn-cloudinary-media-library').click(function(event){
-    event.preventDefault();
-    const cloudinaryCredentials = Array.isArray($(this).data('cloudinaryCredentials'))
-      ? $(this).data('cloudinaryCredentials')
-      : []
+  let cloudinaryButtons = Array.from(document.getElementsByClassName('btn-cloudinary-media-library'));
 
-    let button = $(this).children('button');
-    let buttonClasses = button.attr('class');
-    let buttonInnerHtml = button.prop("innerHTML");
-
-    let objectGroup = $(this).data('objectGroup');
-    let elementId = $(this).attr('id');
-
-    cloudinaryCredentials.map((credential) => {
-      openMediaLibrary(credential, objectGroup, elementId, buttonClasses, buttonInnerHtml);
+  cloudinaryButtons.map((cloudinaryButton) => {
+    cloudinaryButton.addEventListener("click", function(event){
+      event.preventDefault();
+      let buttonClasses = $(this).attr('class');
+      let buttonInnerHtml = $(this).prop("innerHTML");
+      let objectGroup = $(this).data('objectGroup');
+      let elementId = $(this).attr('id');
+      openMediaLibrary(JSON.parse(cloudinaryButton.dataset.cloudinaryCredentials), objectGroup, elementId, buttonClasses, buttonInnerHtml);
     });
   });
 
@@ -90,46 +85,4 @@ define([
     mediaLibrary.storageUid = credential.storageUid;
     mediaLibrary.objectGroup = objectGroup;
   }
-
-  function initializeCloudinaryButtons () {
-    $('.btn-cloudinary-media-library[data-is-initialized="0"]').map((index, element) => {
-
-      let uid = String($(element).parent().parent().parent().data('uid'));
-      let localTable = $(element).parent().parent().parent().data('local-table');
-      let localField = $(element).parent().parent().parent().data('local-field');
-      let radioName = 'data[' + localTable + '][' + uid + '][l10n_state][' + localField + ']';
-
-      let formFieldValue = $('input[name="' + radioName + '"]:checked').val();
-
-      $('input[name="' + radioName + '"]').change(function() {
-        if ($(this).val() === 'parent') {
-          $(element).addClass('invisible');
-        } else if ($(this).val() === 'custom') {
-          $(element).removeClass('invisible');
-        }
-      });
-
-      if (formFieldValue === 'parent') {
-        $(element).addClass('invisible');
-      }
-
-      const cloudinaryCredentials = Array.isArray($(element).data('cloudinaryCredentials'))
-        ? $(element).data('cloudinaryCredentials')
-        : []
-
-      cloudinaryCredentials.map((credential) => {
-          $(element).html('<button class="btn btn-default open-btn mx-1 btn-open-cloudinary btn-open-cloudinary-storage-' + credential.storageUid + '"><span\n' +
-            '                class="t3js-icon icon icon-size-small icon-state-default">\n' +
-            '                <svg class="icon-color">\n' +
-            '                  <use\n' +
-            '                    xlink:href="/typo3/sysext/core/Resources/Public/Icons/T3Icons/sprites/actions.svg#actions-cloud"\n' +
-            '                  ></use>\n' +
-            '                </svg>\n' +
-            '              </span> Image or video from "' + credential.storageName + '"</button>');
-        });
-      });
-  }
-
-  // We trigger a rendering for the normal case
-  initializeCloudinaryButtons()
 });
